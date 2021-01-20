bbbb54045f9266c9b0cea278fa59cf6540ec0d96
Natalia <smolutka15@gmail.com>
2021-01-20 18:34:03 +0100

    17,5 need help with duration filter

--- src/redux/filtersRedux.js
+++ src/redux/filtersRedux.js
@@ -11,10 +11,15 @@ const createActionName = name => `app/${reducerName}/${name}`;
 // action types
 export const CHANGE_PHRASE = createActionName('CHANGE_PHRASE');
 // TODO - add other action types
-
+export const CHANGE_TAG_FILTER = createActionName('CHANGE_TAG_FILTER');
+export const CHANGE_DURATION_TO = createActionName('CHANGE_DURATION_TO');
+export const CHANGE_DURATION_FROM = createActionName('CHANGE_DURATION_FROM');
 // action creators
 export const changeSearchPhrase = payload => ({ payload, type: CHANGE_PHRASE });
 // TODO - add other action creators
+export const changeTagFilter = payload => ({payload, type: CHANGE_TAG_FILTER});
+export const changeDurationTo = payload => ({payload, type: CHANGE_DURATION_TO});
+export const changeDurationFrom = payload => ({payload, type: CHANGE_DURATION_FROM});
 
 // reducer
 export default function reducer(statePart = [], action = {}) {
@@ -25,6 +30,28 @@ export default function reducer(statePart = [], action = {}) {
         searchPhrase: action.payload,
       };
     // TODO - handle other action types
+    case CHANGE_TAG_FILTER:
+      return {
+        ...statePart,
+        tags: action.payload,
+      };
+
+    case CHANGE_DURATION_FROM:
+      return {
+        ...statePart,
+        duration:{
+          ...statePart.duration,
+          from: action.payload,
+        },
+      };
+    case CHANGE_DURATION_TO:
+      return {
+        ...statePart,
+        duration:{
+          ...statePart.duration,
+          to: action.payload,
+        },
+      };
     default:
       return statePart;
   }
