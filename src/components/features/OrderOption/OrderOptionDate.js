41ad02c0591b5df0bf69b6e88d04c0177c651bd6
Natalia <smolutka15@gmail.com>
2021-02-01 15:42:58 +0100

    Fix OrderOptionDate

--- src/components/features/OrderOption/OrderOptionDate.js
+++ src/components/features/OrderOption/OrderOptionDate.js
@@ -1,14 +1,31 @@
 import React, { useState } from 'react';
 import 'react-datepicker/dist/react-datepicker-cssmodules.css';
+import PropTypes from 'prop-types';
 import DatePicker from 'react-datepicker';
 
-const OrderOptionDate = () => {
-  const [startDate, setStartDate] = useState(new Date());
+const OrderOptionDate = ({setOptionValue}) => {
+  const  [startDate, setStartDate] = useState(new Date());
+
+  const dateChange = (date) =>{
+    setStartDate(date);
+    setOptionValue(date);
+  };
+  //console.log(setOptionValue);
+  //console.log(setStartDate);
   return (
-    <DatePicker dateFormat="dd/MM/yyyy" selected={startDate} onChange={date => setStartDate(date)} />
+    <DatePicker
+      dateFormat="dd/MM/yyyy"
+      selected={startDate}
+      onChange={dateChange} />
   );
 };
 
+OrderOptionDate.propTypes ={
+  startDate: PropTypes.node,
+  setStartDate: PropTypes.node,
+  useState: PropTypes.node,
+  setOptionValue: PropTypes.func,
 
+};
 
 export default OrderOptionDate;
