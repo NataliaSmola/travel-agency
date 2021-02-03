29ab0b7dc9083629634df984d6e1b4859f1746ca
Natalia <smolutka15@gmail.com>
2021-02-03 22:42:54 +0100

    Add changes to Text component

--- src/components/features/OrderOption/OrderOptionText.js
+++ src/components/features/OrderOption/OrderOptionText.js
@@ -2,17 +2,19 @@ import React from 'react';
 import styles from './OrderOption.scss';
 import PropTypes from 'prop-types';
 
-const OrderOptionText = ({id}) => (
-  <div className={styles.component}>
+const OrderOptionText = ({id, setOptionValue}) => (
+  <div className={styles.input}>
     <input
       className={styles.input}
       type='text'
       placeholder={(id==='name') ? 'Type your name' : 'Type your contact details'}
+      onChange={event => setOptionValue(event.currentTarget.value)}
     />
   </div>
 );
 OrderOptionText.propTypes ={
   id: PropTypes.node,
+  setOptionValue: PropTypes.func,
 };
 
 export default OrderOptionText;
