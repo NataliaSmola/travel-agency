f4187ec8ba6f3fbd26b02b8ce753d91a47f14abb
Natalia <smolutka15@gmail.com>
2021-02-06 15:33:02 +0100

    TDD - add test 2

--- src/components/features/HappyHourAd/HappyHourAd.test.js
+++ src/components/features/HappyHourAd/HappyHourAd.test.js
@@ -3,8 +3,20 @@ import {shallow} from 'enzyme';
 import HappyHourAd from './HappyHourAd';
 
 describe('Component HappyHourAd', () => {
+
+  const select = {
+    title: '.title',
+    promoDescription: '.promoDescription',
+  };
+
   it('Should render without crashing', () =>{
     const component = shallow(<HappyHourAd/>);
     expect (component).toBeTruthy();
+  });
+
+  it('has correct compnent with title and countdown timing', () => {
+    const component = shallow(<HappyHourAd/>);
+    expect(component.exists(select .title)).toEqual(true);
+    expect(component.exists(select .promoDescription)).toEqual(true);
   });
 });
